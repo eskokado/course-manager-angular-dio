c5366d3d8a083f55a36d8c815a0e8c603ae03d3c
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-14 09:46:59 -0300

    Implementando component star

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -14,7 +14,7 @@ export class CourseListComponent implements OnInit{
             {
                 id: 1,
                 name: 'Angular: Forms',
-                imageUrl: '',
+                imageUrl: '/assets/images/forms.png',
                 price: 99.99,
                 code: 'XPS-8796',
                 duration: 120,
@@ -24,7 +24,7 @@ export class CourseListComponent implements OnInit{
             {
               id: 2,
               name: 'Angular: HTTP',
-              imageUrl: '',
+              imageUrl: '/assets/images/http.png',
               price: 45.00,
               code: 'LPL-1234',
               duration: 80,
